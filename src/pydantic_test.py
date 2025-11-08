41d57f7fecbaf174ea2268f354fe941e1d70d258
elcu <merlickg@gmail.com>
2025-11-08 10:15:46 +0100

    Initial commit

---
+++ src/pydantic_test.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+class User(BaseModel):
+    name: str
+    email: EmailStr
+    account_id: int
+
+    @field_validator("account_id")
+    def validate_account_id(cls, value):
+        if value < 0:
+            raise ValueError(f"account_id must be positive: {value}")
+        return value
+
+user = User(
+    name="Tomas",
+    email="tomas@gmail.com",
+    account_id=1
+)
+
+print(user.model_dump_json())
